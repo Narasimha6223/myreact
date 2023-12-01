05ddfc144a20bbc0d88bb2ef8b819e4323c0c397
Narasimha <vaddalanarasimha@gmail.com>
2023-12-01 19:03:36 +0530

    ABout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About(props) {
     props.setloading(false)
     props.setcategory("About Us")
     return (
-        <div id='aboutbox' style={{textAlign:"justify",  margin:"50px auto" , fontSize:"30px" , fontFamily: `Open Sans, sans-serif`}}>
+        <div id='aboutbox' style={{textAlign:"justify",  margin:"50px auto" , fontSize:"30px" , fontFamily: `Open Sans, sans-serif` , marginTop:0}}>
             <p>  Mini News is a News App Built By Me.
                 This is Made Using React JS. This is Made Just to Learn React using Project Based Learning.
                 Still Mini News is a Useful app, You can Read news from different sources fetched from News Api.
